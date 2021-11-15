ae3501b13b6772eb010581236ffaa04f5e2042cc
YANGZhaochen2002 <93120280+YANGZhaochen2002@users.noreply.github.com>
2021-11-15 12:11:21 +0800

    Update Group.java

--- src/Code/model/shape/Group.java
+++ src/Code/model/shape/Group.java
@@ -3,14 +3,16 @@ package Code.model.shape;
 import java.util.HashMap;
 
 public class Group extends Shape{
-    private String name;
-    private HashMap<String, Shape> groupShape;
+    String name;
+    public List<Shape> Arr;//之前的List（可以存在group在里面）
+    public List<Shape> All_Shape;//仅包含单个Shape的List
     private int thisZOrder;
 
-    public Group(String name, HashMap<String, Shape> groupShape) {
-        this.name = name;
-        this.groupShape = groupShape;
-        thisZOrder = super.getzOrder();
+    public Group(String name,List<Shape> Arr,List<Shape> All_Shape) {//构造器
+        Arr = new ArrayList<>();
+        this.Arr=Arr;
+        this.name=name;
+        this.All_Shape =All_Shape ;
     }
     public Rectangle boundingBox(){
         return new Rectangle();
@@ -18,11 +20,22 @@ public class Group extends Shape{
 
     //judge whether this object contains the point(x,y)
     public boolean isContainPoint(double x, double y){
-        return false;
+        for(Shape a:All_Shape ){
+            if(a.isContainPoint(x,y) ){
+                return true;
+            }
+        }
+        return false ;
     }
 
     //move dx and dy for this objective
     public void move(double dx, double dy){
+        for(Shape a:All_Shape){
+            a.move(dx,dy);
+        }
+        for(Shape b:Arr ){
+            b.move(dx,dy);
+        }
 
     }
 
@@ -33,10 +46,6 @@ public class Group extends Shape{
 
     public String getName() {
         return name;
-    }
-
-    public HashMap<String, Shape> getGroupShape() {
-        return groupShape;
     }
 
     public int getThisZOrder() {
